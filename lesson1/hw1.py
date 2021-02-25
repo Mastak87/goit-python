3fdf7ba70de56ab25e4465601351b72d2679b4a4
Andrii Mastykash <mastykash87@gmail.com>
2021-02-25 21:53:31 +0100

    hw1

--- lesson1/hw1.py
+++ lesson1/hw1.py
@@ -1,2 +1,3 @@
-Name = input('Enter your name please:')
+Name = input('Enter your name please:')     # Variable Name
+# Print 'HEllo' with name in variable Name
 print('Hello {}'.format(Name))
--- lesson1/hw2.py
+++ lesson1/hw2.py
@@ -1,7 +1,7 @@
-a = int(input("Enter a:"))
-b = int(input("Enter b:"))
-c = int(input("Enter c:"))
-D = b ** 2 - 4 * a * c
-x1 = (-b + D**0.5) / 2*a
-x2 = (-b - D**0.5) / 2*a
-print(x1, x2, sep=';')
+a = int(input("Enter a:"))          # Variable a
+b = int(input("Enter b:"))          # Variable b
+c = int(input("Enter c:"))          # Variable c
+D = b ** 2 - 4 * a * c              # Сalculation of discriminant
+x1 = (-b + D**0.5) / 2*a            # Calculation of the first root
+x2 = (-b - D**0.5) / 2*a            # Calculation of the second root
+print(x1, x2, sep=';')              # Printing of roots
