3f882456c7c5f877c1d21f2f9a62067c4a18d85f
Andrii Mastykash <mastykash87@gmail.com>
2021-02-25 21:57:58 +0100

    hw1

--- lesson1/hw1.py
+++ lesson1/hw1.py
@@ -1,3 +1,3 @@
-Name = input('Enter your name please:')     # Variable Name
-# Print 'HEllo' with name in variable Name
+Name = input('Enter your name please:')  # Variable Name
 print('Hello {}'.format(Name))
+# Print 'HEllo' with name in variable Name
