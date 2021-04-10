15364dba45853dd79ac481c5aec91d9c129ea869
Andrii Mastykash <mastykash87@gmail.com>
2021-04-10 22:50:17 +0200

    home work 4

--- lesson1/hw2.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter a:"))          # Variable a
-b = int(input("Enter b:"))          # Variable b
-c = int(input("Enter c:"))          # Variable c
-D = b ** 2 - 4 * a * c              # Сalculation of discriminant
-x1 = (-b + D**0.5) / 2*a            # Calculation of the first root
-x2 = (-b - D**0.5) / 2*a            # Calculation of the second root
-print(x1, x2, sep=';')              # Printing of roots
