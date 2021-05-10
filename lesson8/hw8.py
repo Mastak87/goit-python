cd945ff3c7df92d4befbb989d3335af6d6d7c252
Andrii Mastykash <mastykash87@gmail.com>
2021-05-10 21:47:07 +0200

    hw8 fixed

--- lesson8/hw8.py
+++ lesson8/hw8.py
@@ -1,27 +1,50 @@
 from datetime import datetime,timedelta
 
 
-
 users = [{'Andrii': datetime(year=2021, month=8, day=9)}, {'Petro': datetime(year=2021, month=5, day=21)},
          {'Maksym': datetime(year=2021, month=7, day=20)}, {'Ivan': datetime(year=2021, month=5, day=21)},
          {'Bogdan': datetime(year=2021, month=5, day=15)}, {'Oleg':datetime(year=2021, month=5, day=18)}]
 
-next_week = datetime.now() + timedelta(weeks=1) - timedelta(days=1)
-next_week_2 = next_week + timedelta(weeks=1) - timedelta(days=1)
-week = timedelta(days=7)
-week_zero = timedelta(days=0)
 
 def congratulate(users):
-    week_birthdays = ''
+
+    first_day = datetime.now() + timedelta(weeks=1) - timedelta(days=1)
+    last_day = first_day + timedelta(weeks=1) - timedelta(days=1)
+    week_first_day = timedelta(days=0)
+    week_last_day = timedelta(days=7)
+
+    week_birthdays = {}
+
     for i in users:
 
         for k, v in i.items():
             name = k
             birthday = v.strftime("%A")
-            if next_week_2 - v < week and next_week_2 - v > week_zero:
-                week_birthdays += f'{birthday}: {name}\n'
+            if last_day - v < week_last_day and last_day - v > week_first_day:
+                week_birthdays[name] = birthday
 
-    return week_birthdays
+    sort_week_birthdays = {}
+
+    for names, day in week_birthdays.items():
+        char = day
+        if char not in sort_week_birthdays:
+            sort_week_birthdays[char] = []
+        sort_week_birthdays[char].append(names)
+
+    week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    sort_by_day = {}
+
+    for day in week_days:
+        if day in sort_week_birthdays:
+            sort_by_day[day] = sort_week_birthdays[day]
+
+    birthday_list = ''
+
+    for k,v in sort_by_day.items():
+        names = ', '.join(v)
+        birthday_list += f'{k}: {names}\n'
+
+    return birthday_list
 
 if __name__ == '__main__':
     print(congratulate(users))
